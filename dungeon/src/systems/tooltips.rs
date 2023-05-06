48e34b6089f09aec3cb69e9622d02eb3b7a34be0
Averrous Saloom <averous009@gmail.com>
2023-05-07 00:11:32 +0700

    feat: show toolips when nearby

--- dungeon/src/systems/tooltips.rs
+++ dungeon/src/systems/tooltips.rs
@@ -1,9 +1,11 @@
+
 use crate::prelude::*;
 
 #[system]
 #[read_component(Point)]
 #[read_component(Name)]
 #[read_component(Health)]
+#[read_component(Player)]
 pub fn tooltips(
   ecs: &SubWorld,
   #[resource] mouse_pos: &Point,
@@ -20,15 +22,33 @@ pub fn tooltips(
     .filter(|(_,pos,_)| **pos == map_pos)
     .for_each(|(entity, _, name)| {
       let screen_pos = *mouse_pos * 4;
-      let display = if let Ok(health) = ecs.entry_ref(*entity)
-        .unwrap()
-        .get_component::<Health>()
-      {
-        format!("{} [{} / {}]", &name.0, health.current, health.max)
-      } else {
-        name.0.clone()
-      };
-      draw_batch.print(screen_pos, display);
+      draw_batch.print(screen_pos, display_health_wording(ecs, entity, name));
     });
+  
+  let mut players = <(&Player, &Point)>::query();
+  
+  players
+    .iter(ecs)
+    .for_each(|(_, player_pos)| {
+      positions.iter(ecs)
+        .filter(|(_,pos,_)| {
+          let distance = f32::sqrt((player_pos.x - pos.x).pow(2) as f32 + (player_pos.y - pos.y).pow(2) as f32);
+          distance < 5.0
+        })
+        .for_each(|(entity, pos, name)| {
+          let screen_pos = (pos.clone() - offset) * 4;
+          draw_batch.print(screen_pos, display_health_wording(ecs, entity, name));
+    })});
   draw_batch.submit(10100).expect("Batch error");
+}
+
+fn display_health_wording(ecs: &SubWorld, entity: &Entity, name: &Name) -> String {
+  if let Ok(health) = ecs.entry_ref(*entity)
+    .unwrap()
+    .get_component::<Health>()
+  {
+     return format!("{} [{} / {}]", &name.0, health.current, health.max);
+  } else {
+     return name.0.clone();
+  }
 }
